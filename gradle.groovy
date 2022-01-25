3d7ad358c05f755cb48fd98382b72ae51906ab30
Ricardo <ricardo-sebastian@live.com>
2022-01-24 22:24:38 -0300

    updated JENKINSFILE, gradle groovy

--- gradle.groovy
+++ gradle.groovy
@@ -1,31 +1,31 @@
 def call() {
     stage('Build & Test') {
         environment {
-            STAGE = "${env.STAGE_NAME}"
+            STAGE = ${env.STAGE_NAME}
         }
         sh './gradlew clean build'
     }
     stage('Sonar') {
         environment {
-            STAGE = "${env.STAGE_NAME}"
+            STAGE = ${env.STAGE_NAME}
         }
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Run') {
         environment {
-            STAGE = "${env.STAGE_NAME}"
+            STAGE = ${env.STAGE_NAME}
         }
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Test') {
         environment {
-            STAGE = "${env.STAGE_NAME}"
+            STAGE = ${env.STAGE_NAME}
         }
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Nexus') {
         environment {
-            STAGE = "${env.STAGE_NAME}"
+            STAGE = ${env.STAGE_NAME}
         }
         println "Stage: ${env.STAGE_NAME}"
     }
