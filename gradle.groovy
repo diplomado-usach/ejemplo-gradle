4c065ab8e524351750250f136b0d013369f22a89
Ricardo <ricardo-sebastian@live.com>
2022-01-24 23:59:18 -0300

    updated gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -1,23 +1,23 @@
-
+STAGE= ""
 def call() {
     stage('Build & Test') {
-        env['STAGE'] = "Build & Test"
+        STAGE = "Build & Test"
         sh './gradlew clean build'
     }
     stage('Sonar') {
-        env['STAGE'] = "Sonar"
+        STAGE = "Sonar"
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Run') {
-        env['STAGE'] = "Run"
+        STAGE = "Run"
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Test') {
-        env['STAGE'] = "Test"
+        STAGE = "Test"
         println "Stage: ${env.STAGE_NAME}"
     }
     stage('Nexus') {
-        env['STAGE'] = "Nexus"
+        STAGE = "Nexus"
         println "Stage: ${env.STAGE_NAME}"
     }
 }
