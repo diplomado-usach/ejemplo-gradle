94b35efc63f56237cecd144dece34196c13819cf
Ricardo Quiroga <31752537+ricardoseb@users.noreply.github.com>
2022-01-28 19:27:51 -0300

    Update DevOpsUsach2020ApplicationTests.java

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -3,7 +3,6 @@ package com.devopsusach2020;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
